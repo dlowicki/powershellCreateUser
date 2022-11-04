e8bd8918a9a1a5b6efb45e28c58fcb75f4b5ecdb
dlowicki <44500521+dlowicki@users.noreply.github.com>
2022-11-04 23:54:52 +0100

    Update Version 0.0.4

--- loadData.js
+++ loadData.js
@@ -1,26 +1,56 @@
 function loadErstellen() {
     $('#widget').empty();
 
-    $('#widget').append('<div class="container-small" id="cs1"></div><div class="container-small" id="cs2"></div><div class="container-small" id="cs3"></div><div class="container-small" id="cs4"></div><div class="container-small" id="cs5"></div>');
+    $('#widget').append('<div class="container-small" id="cs1"></div><div class="container-small" id="cs2"></div><div class="container-small" id="cs3"></div><div class="container-small" id="cs4"></div><div class="container-small" id="cs5"></div><div class="container-small" id="cs6"></div>');
 
     // Lade Container 1
-    $('#cs1').append('<div class="m-textbox" id="tb_vorname"><label>Vorname</label><input type="text" id="m_vn" placeholder="Vorname"></div>');
-    $('#cs1').append('<div class="m-textbox" id="tb_nachname"><label>Nachname</label><input type="text" id="m_nn" placeholder="Nachname"></div>');
+    $('#cs1').append('<h3>Personen-Daten</h3>');
+    $('#cs1').append('<div class="m-flex-wrap"></div>');
+    $('#cs1 .m-flex-wrap').append('<div class="m-textbox" id="tb_vorname"><label>Vorname</label><input type="text" id="m_vn" placeholder="Vorname"></div>');
+    $('#cs1 .m-flex-wrap').append('<div class="m-textbox" id="tb_nachname"><label>Nachname</label><input type="text" id="m_nn" placeholder="Nachname"></div>');
 
     // Lade Container 2
-    $('#cs2').append('<div class="m-dropdown" id="dd_firma"><label>Firma</label><select id="firma"><option value="SJ">Schrauben-Jäger</option><option value="WJ">Werkzeug-Jäger</option></select></div>');
-    $('#cs2').append('<div class="m-dropdown" id="dd_standort"><label>Standort</label><select id="standort"><option value="karlsruhe">Karlsruhe</option><option value="illingen">Illingen</option><option value="landsberg">Landsberg</option><option value="maulburg">Maulburg</option><option value="viernheim">Viernheim</option><option value="willstaett">Willstätt</option></select></div>');
+    $('#cs2').append('<h3>Firmen-Daten</h3>');
+    $('#cs2').append('<div class="m-flex-wrap"></div>');
+    $('#cs2 .m-flex-wrap').append('<div class="m-dropdown" id="dd_firma"><label>Firma</label><select id="firma"><option value="SJ">Schrauben-Jäger</option><option value="WJ">Werkzeug-Jäger</option></select></div>');
+    $('#cs2 .m-flex-wrap').append('<div class="m-dropdown" id="dd_standort"><label>Standort</label><select id="standort"><option value="karlsruhe">Karlsruhe</option><option value="illingen">Illingen</option><option value="landsberg">Landsberg</option><option value="maulburg">Maulburg</option><option value="viernheim">Viernheim</option><option value="willstaett">Willstätt</option></select></div>');
+    $('#cs2 .m-flex-wrap').append('<div class="m-dropdown" id="dd_standort"><label>Abteilung</label><select id="standort"><option value="karlsruhe">Karlsruhe</option><option value="illingen">Illingen</option><option value="landsberg">Landsberg</option><option value="maulburg">Maulburg</option><option value="viernheim">Viernheim</option><option value="willstaett">Willstätt</option></select></div>');
 
     // Lade Container 3
-    $('#cs3').append('<div class="m-textbox" id="tb_durchwahl"><label>Durchwahl</label><input type="text" id="m_dw" placeholder="Durchwahl"></div>');
-    $('#cs3').append('<div class="m-textbox" id="tb_mobile"><label>Mobile-Nummer</label><input type="text" id="m_mn" placeholder="Mobile-Nummer"></div>');
+    $('#cs3').append('<h3>Abteilungs-Daten</h3>');
+    $('#cs3').append('<div class="m-flex-wrap"></div>');
+    $('#cs3 .m-flex-wrap').append('<div class="m-textbox" id="tb_durchwahl"><label>Durchwahl</label><input type="text" id="m_dw" placeholder="Durchwahl"></div>');
+    $('#cs3 .m-flex-wrap').append('<div class="m-textbox" id="tb_mobile"><label>Mobile-Nummer</label><input type="text" id="m_mn" placeholder="Mobile-Nummer"></div>');
+    $('#cs3 .m-flex-wrap').append('<div class="m-dropdown" id="dd_vorgesetzter"><label>Vorgesetzter</label><select id="vorgesetzter"><option value="Vorgesetzter 1">Vorgesetzter 1</option><option value="Vorgesetzter 2">Vorgesetzter 2</option><option value="Vorgesetzter 3">Vorgesetzter 3</option><option value="Vorgesetzter 4">Vorgesetzter 4</option><option value="Vorgesetzter 5">Vorgesetzter 5</option><option value="Vorgesetzter 6">Vorgesetzter 6</option></select></div>');
+    
 
     // Lade Container 4
-    $('#cs4').append('<div class="m-dropdown" id="dd_vorgesetzter"><label>Vorgesetzter</label><select id="vorgesetzter"><option value="Vorgesetzter 1">vorgesetzter 1</option><option value="Vorgesetzter 2">Vorgesetzter 2</option><option value="Vorgesetzter 3">Vorgesetzter 3</option><option value="Vorgesetzter 4">Vorgesetzter 4</option><option value="Vorgesetzter 5">Vorgesetzter 5</option><option value="Vorgesetzter 6">Vorgesetzter 6</option></select></div>');
-    $('#cs4').append('<div class="m-checkbox" id="cb_gruppen"><div class="cb"><label>Gruppe1</label><label class="switch"><input type="checkbox" id="m_gp1"><span class="slider round"></span></label></div><div class="cb"><label>Gruppe2</label><label class="switch"><input type="checkbox" id="m_gp2"><span class="slider round"></span></label></div></div>');
+    $('#cs4').append('<h3>Hardware</h3>');
+    $('#cs4').append('<div class="m-flex-wrap"></div>');
+    $('#cs4 .m-flex-wrap').append('<div class="m-dropdown" id="dd_vorgesetzter"><label>Vorgesetzter</label><select id="vorgesetzter"><option value="Vorgesetzter 1">Vorgesetzter 1</option><option value="Vorgesetzter 2">Vorgesetzter 2</option><option value="Vorgesetzter 3">Vorgesetzter 3</option><option value="Vorgesetzter 4">Vorgesetzter 4</option><option value="Vorgesetzter 5">Vorgesetzter 5</option><option value="Vorgesetzter 6">Vorgesetzter 6</option></select></div>');
+    
 
     // Lade Container 5
-    $('#cs5').append('<div class="m-checkbox" id="cb_lizenz"><div class="cb"><label>Office Basic</label><label class="switch"><input type="checkbox" class="officeLic" id="m_licBasic"><span class="slider round"></span></label></div><div class="cb"><label>Office Premium</label><label class="switch"><input type="checkbox" class="officeLic" id="m_licPremium"><span class="slider round"></span></label></div></div>');    
+    $('#cs5').append('<h3>Gruppen</h3>');
+    $('#cs5').append('<div class="m-flex-wrap"></div>');
+    $('#cs5 .m-flex-wrap').append('<div class="m-checkbox" id="cb_gruppen"><div class="cb"><label>Gruppe1</label><label class="switch"><input type="checkbox" id="m_gp1"><span class="slider round"></span></label></div></div>');
+    $('#cs5 .m-flex-wrap').append('<div class="m-checkbox" id="cb_gruppen"><div class="cb"><label>Gruppe2</label><label class="switch"><input type="checkbox" id="m_gp2"><span class="slider round"></span></label></div></div>');
+    $('#cs5 .m-flex-wrap').append('<div class="m-checkbox" id="cb_gruppen"><div class="cb"><label>Gruppe3</label><label class="switch"><input type="checkbox" id="m_gp3"><span class="slider round"></span></label></div></div>');
+    $('#cs5 .m-flex-wrap').append('<div class="m-checkbox" id="cb_gruppen"><div class="cb"><label>Gruppe1</label><label class="switch"><input type="checkbox" id="m_gp1"><span class="slider round"></span></label></div></div>');
+    $('#cs5 .m-flex-wrap').append('<div class="m-checkbox" id="cb_gruppen"><div class="cb"><label>Gruppe2</label><label class="switch"><input type="checkbox" id="m_gp2"><span class="slider round"></span></label></div></div>');
+    $('#cs5 .m-flex-wrap').append('<div class="m-checkbox" id="cb_gruppen"><div class="cb"><label>Gruppe3</label><label class="switch"><input type="checkbox" id="m_gp3"><span class="slider round"></span></label></div></div>');
+    $('#cs5 .m-flex-wrap').append('<div class="m-checkbox" id="cb_gruppen"><div class="cb"><label>Gruppe1</label><label class="switch"><input type="checkbox" id="m_gp1"><span class="slider round"></span></label></div></div>');
+    $('#cs5 .m-flex-wrap').append('<div class="m-checkbox" id="cb_gruppen"><div class="cb"><label>Gruppe2</label><label class="switch"><input type="checkbox" id="m_gp2"><span class="slider round"></span></label></div></div>');
+    $('#cs5 .m-flex-wrap').append('<div class="m-checkbox" id="cb_gruppen"><div class="cb"><label>Gruppe3</label><label class="switch"><input type="checkbox" id="m_gp3"><span class="slider round"></span></label></div></div>');
+    $('#cs5 .m-flex-wrap').append('<div class="m-checkbox" id="cb_gruppen"><div class="cb"><label>Gruppe1</label><label class="switch"><input type="checkbox" id="m_gp1"><span class="slider round"></span></label></div></div>');
+    $('#cs5 .m-flex-wrap').append('<div class="m-checkbox" id="cb_gruppen"><div class="cb"><label>Gruppe2</label><label class="switch"><input type="checkbox" id="m_gp2"><span class="slider round"></span></label></div></div>');
+    $('#cs5 .m-flex-wrap').append('<div class="m-checkbox" id="cb_gruppen"><div class="cb"><label>Gruppe3</label><label class="switch"><input type="checkbox" id="m_gp3"><span class="slider round"></span></label></div></div>');
+
+
+    // Lade Container 6
+    $('#cs6').append('<h3>Office-Paket</h3>');
+    $('#cs6').append('<div class="m-flex-wrap"></div>');
+    $('#cs6 .m-flex-wrap').append('<div class="m-checkbox" id="cb_lizenz"><div class="cb"><label>Office Basic</label><label class="switch"><input type="checkbox" class="officeLic" id="m_licBasic"><span class="slider round"></span></label></div><div class="cb"><label>Office Premium</label><label class="switch"><input type="checkbox" class="officeLic" id="m_licPremium"><span class="slider round"></span></label></div></div>');    
 
 
     // Lade Buttons
@@ -54,6 +84,7 @@ function fillDataBearbeiten(str) {
     if(dc == false){
         // Lade Daten mit str (str = id)
         $('#widget').append('<div class="container-small" id="cs1"></div>');
+        $('#cs1').append('<h3>Personen-Daten</h3>');
         $('#cs1').append('<div class="m-textbox" id="tb_vorname"><label>Vorname</label><input type="text" id="m_vn" placeholder="Vorname" value="'+ str +'"></div>');
         $('#cs1').append('<div class="m-textbox" id="tb_nachname"><label>Nachname</label><input type="text" id="m_nn" placeholder="Nachname"></div>');
     
@@ -66,10 +97,11 @@ function fillDataBearbeiten(str) {
         $('#widget').append('<div class="container-small" id="cs3"></div>');
         $('#cs3').append('<div class="m-textbox" id="tb_durchwahl"><label>Durchwahl</label><input type="text" id="m_dw" placeholder="Durchwahl"></div>');
         $('#cs3').append('<div class="m-textbox" id="tb_mobile"><label>Mobile-Nummer</label><input type="text" id="m_mn" placeholder="Mobile-Nummer"></div>');
-    
+        $('#cs3').append('<div class="m-dropdown" id="dd_vorgesetzter"><label>Vorgesetzter</label><select id="vorgesetzter"><option value="Vorgesetzter 1">vorgesetzter 1</option><option value="Vorgesetzter 2">Vorgesetzter 2</option><option value="Vorgesetzter 3">Vorgesetzter 3</option><option value="Vorgesetzter 4">Vorgesetzter 4</option><option value="Vorgesetzter 5">Vorgesetzter 5</option><option value="Vorgesetzter 6">Vorgesetzter 6</option></select></div>');
+        
+
         // Lade Container 4
         $('#widget').append('<div class="container-small" id="cs4"></div>');
-        $('#cs4').append('<div class="m-dropdown" id="dd_vorgesetzter"><label>Vorgesetzter</label><select id="vorgesetzter"><option value="Vorgesetzter 1">vorgesetzter 1</option><option value="Vorgesetzter 2">Vorgesetzter 2</option><option value="Vorgesetzter 3">Vorgesetzter 3</option><option value="Vorgesetzter 4">Vorgesetzter 4</option><option value="Vorgesetzter 5">Vorgesetzter 5</option><option value="Vorgesetzter 6">Vorgesetzter 6</option></select></div>');
         $('#cs4').append('<div class="m-checkbox" id="cb_gruppen"><div class="cb"><label>Gruppe1</label><label class="switch"><input type="checkbox" id="m_gp1"><span class="slider round"></span></label></div><div class="cb"><label>Gruppe2</label><label class="switch"><input type="checkbox" id="m_gp2"><span class="slider round"></span></label></div></div>');
     
         // Lade Container 5
