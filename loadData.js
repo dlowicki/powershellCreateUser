37e7d4d87ec3b4a326e5cdf00293893ac838ef7c
dlowicki <44500521+dlowicki@users.noreply.github.com>
2022-11-09 22:33:58 +0100

    Update Version 0.1.2

--- loadData.js
+++ loadData.js
@@ -14,9 +14,11 @@ if(window.location.href.includes('index.html')) {
 
     var link = new String(cURL.searchParams.get("edit"));
     var editMode = false;
-    if(link){ if(link.length == 12){ editMode = true; } }
+    if(link){ if(link.length == 12){ editMode = true; } else {
+      // Abbruch von edit, da Datei nicht existieren kann
+    } }
   
-    console.log('[JSON] Daten wurden geladen');    
+    console.log('[JSON] Daten wurden geladen'); 
     
     $('#widget').empty();
     $('#widget').append('<div class="container-small" id="cs1"></div>');
@@ -31,21 +33,21 @@ if(window.location.href.includes('index.html')) {
     $('#cs1').append('<div class="container-headline" id="cs1-headline"><h3>Personen-Daten</h3></div>');
     $('#cs1').append('<div class="m-flex-wrap"></div>');
     $('#cs1 .m-flex-wrap').append('<div class="m-textbox" id="tb_vorname"><label>Vorname</label><input type="text" data-id="m-1" id="m_vn" placeholder="Vorname"></div>');
-    $('#cs1 .m-flex-wrap').append('<div class="m-textbox" id="tb_nachname"><label>Nachname</label><input type="text" id="m_nn" placeholder="Nachname"></div>');
-    $('#cs1 .m-flex-wrap').append('<div class="m-textbox" id="tb_nachname"><label>Eintrittsdatum</label><input type="date" id="m_date" placeholder="Datum"></div>');
+    $('#cs1 .m-flex-wrap').append('<div class="m-textbox" id="tb_nachname"><label>Nachname</label><input type="text" data-id="m-2" id="m_nn" placeholder="Nachname"></div>');
+    $('#cs1 .m-flex-wrap').append('<div class="m-textbox" id="tb_nachname"><label>Eintrittsdatum</label><input type="date" data-id="m-3" id="m_date" placeholder="Datum"></div>');
   
     // Lade Container 2 // Firmen-Daten
     $('#cs2').append('<div class="container-headline" id="cs2-headline"><h3>Firmen-Daten</h3></div>');
     $('#cs2').append('<div class="m-flex-wrap"></div>');
-    $('#cs2 .m-flex-wrap').append('<div class="m-dropdown" id="dd_firma"><label>Firma</label><select id="firma"></select></div>');
+    $('#cs2 .m-flex-wrap').append('<div class="m-dropdown" id="dd_firma"><label>Firma</label><select data-id="m-4" id="firma"></select></div>');
     $.each(items['ar-firma'], function(key,val){
       $('#firma').append('<option value="'+val+'">'+val+'</option>');
     });
-    $('#cs2 .m-flex-wrap').append('<div class="m-dropdown" id="dd_standort"><label>Standort</label><select id="standort"></select></div>');
+    $('#cs2 .m-flex-wrap').append('<div class="m-dropdown" id="dd_standort"><label>Standort</label><select data-id="m-5" id="standort"></select></div>');
     $.each(items['ar-Standort'], function(key,val){
       $('#standort').append('<option value="'+val+'">'+val+'</option>');
     });
-    $('#cs2 .m-flex-wrap').append('<div class="m-dropdown" id="dd_abteilung"><label>Abteilung</label><select id="abteilung"></div>');
+    $('#cs2 .m-flex-wrap').append('<div class="m-dropdown" id="dd_abteilung"><label>Abteilung</label><select data-id="m-6" id="abteilung"></div>');
     $.each(items['ar-Abteilung'], function(key,val){
       $('#abteilung').append('<option value="'+val+'">'+val+'</option>');
     });
@@ -53,9 +55,9 @@ if(window.location.href.includes('index.html')) {
     // Lade Container 3 // Abteilungs-Daten
     $('#cs3').append('<div class="container-headline" id="cs3-headline"><h3>Abteilungs-Daten</h3></div>');
     $('#cs3').append('<div class="m-flex-wrap"></div>');
-    $('#cs3 .m-flex-wrap').append('<div class="m-textbox" id="tb_durchwahl"><label>Durchwahl</label><input type="text" id="m_dw" placeholder="Durchwahl"></div>');
-    $('#cs3 .m-flex-wrap').append('<div class="m-textbox" id="tb_mobile"><label>Mobile-Nummer</label><input type="text" id="m_mn" placeholder="Mobile-Nummer"></div>');
-    $('#cs3 .m-flex-wrap').append('<div class="m-dropdown" id="dd_vorgesetzter"><label>Vorgesetzter</label><select id="vorgesetzter"></select></div>');
+    $('#cs3 .m-flex-wrap').append('<div class="m-textbox" id="tb_durchwahl"><label>Durchwahl</label><input type="text" data-id="m-7" id="m_dw" placeholder="Durchwahl"></div>');
+    $('#cs3 .m-flex-wrap').append('<div class="m-textbox" id="tb_mobile"><label>Mobile-Nummer</label><input type="text" data-id="m-8" id="m_mn" placeholder="Mobile-Nummer"></div>');
+    $('#cs3 .m-flex-wrap').append('<div class="m-dropdown" id="dd_vorgesetzter"><label>Vorgesetzter</label><select data-id="m-9" id="vorgesetzter"></select></div>');
     $.each(items['ar-Vorgesetzter'], function(key,val){
       $('#vorgesetzter').append('<option value="'+val+'">'+val+'</option>');
     });
@@ -63,33 +65,33 @@ if(window.location.href.includes('index.html')) {
     // Lade Container 4 // Hardware
     $('#cs4').append('<div class="container-headline" id="cs4-headline"><h3>Hardware</h3></div>');
     $('#cs4').append('<div class="m-flex-wrap"></div>');
-    $('#cs4 .m-flex-wrap').append('<div class="m-dropdown" id="dd_pc_laptop"><label>Laptop/PC</label><select id="pc_laptop"></select></div>');
+    $('#cs4 .m-flex-wrap').append('<div class="m-dropdown" id="dd_pc_laptop"><label>Laptop/PC</label><select data-id="m-10" id="pc_laptop"></select></div>');
     $.each(items['ar-pc-laptop'], function(key,val){
       $('#pc_laptop').append('<option value="'+val+'">'+val+'</option>');
     });
-    $('#cs4 .m-flex-wrap').append('<div class="m-dropdown" id="dd_tastatur_maus"><label>Tastatur/Maus</label><select id="tastatur_maus"></select></div>');
+    $('#cs4 .m-flex-wrap').append('<div class="m-dropdown" id="dd_tastatur_maus"><label>Tastatur/Maus</label><select data-id="m-11" id="tastatur_maus"></select></div>');
     $.each(items['ar-tastatur-maus'], function(key,val){
       $('#tastatur_maus').append('<option value="'+val+'">'+val+'</option>');
     });
-    $('#cs4 .m-flex-wrap').append('<div class="m-dropdown" id="dd_headset"><label>Headset</label><select id="headset"></select></div>');
+    $('#cs4 .m-flex-wrap').append('<div class="m-dropdown" id="dd_headset"><label>Headset</label><select data-id="m-12" id="headset"></select></div>');
     $.each(items['ar-headset'], function(key,val){
       $('#headset').append('<option value="'+val+'">'+val+'</option>');
     });
-    $('#cs4 .m-flex-wrap').append('<div class="m-dropdown" id="dd_monitore"><label>Monitore</label><select id="monitore"></select></div>');
+    $('#cs4 .m-flex-wrap').append('<div class="m-dropdown" id="dd_monitore"><label>Monitore</label><select data-id="m-13" id="monitore"></select></div>');
     $.each(items['ar-monitore'], function(key,val){
       $('#monitore').append('<option value="'+val+'">'+val+'</option>');
     });
-    $('#cs4 .m-flex-wrap').append('<div class="m-checkbox" id="cb_docking"><div class="cb"><label>Docking-Station</label><label class="switch"><input type="checkbox" id="m_gp3"><span class="slider round"></span></label></div></div>');
-    $('#cs4 .m-flex-wrap').append('<div class="m-checkbox" id="cb_handy"><div class="cb"><label>Geschäfts-Handy</label><label class="switch"><input type="checkbox" id="m_gp3"><span class="slider round"></span></label></div></div>');
+    $('#cs4 .m-flex-wrap').append('<div class="m-checkbox" id="cb_docking"><div class="cb"><label>Docking-Station</label><label class="switch"><input type="checkbox" data-id="m-14"><span class="slider round"></span></label></div></div>');
+    $('#cs4 .m-flex-wrap').append('<div class="m-checkbox" id="cb_handy"><div class="cb"><label>Geschäfts-Handy</label><label class="switch"><input type="checkbox" data-id="m-15"><span class="slider round"></span></label></div></div>');
    
   
   
     // Lade Container 5 // Gruppen
     $('#cs5').append('<div class="container-headline" id="cs5-headline"><h3>Software</h3></div>');
     $('#cs5').append('<div class="m-flex-wrap" id="cb_software"></div>');
-    var counter = 1;
+    var counter = 16;
     $.each(items['ar-software'], function(key,val){
-      $('#cb_software').append('<div class="m-checkbox"><div class="cb"><label>'+val+'</label><label class="switch"><input type="checkbox" id="m_gp'+counter+'"><span class="slider round"></span></label></div></div>');
+      $('#cb_software').append('<div class="m-checkbox"><div class="cb"><label>'+val+'</label><label class="switch"><input type="checkbox" data-id="m-'+counter+'"><span class="slider round"></span></label></div></div>');
       counter++;
     });
   
@@ -117,11 +119,52 @@ if(window.location.href.includes('index.html')) {
     }
     $('.container-buttons').append('<button id="bt_en" onclick="sendData()">EDV Senden</button>');
 
-    var ajaxObj = loadEditData(link);
-    var ajaxResponse = ajaxObj.responseText;
-    var splitted = ajaxResponse.split(';');
-    
-  
+    if(editMode == true){
+      var ajaxObj = loadEditData(link);
+      var ajaxResponse = ajaxObj.responseText;
+      var splitted = ajaxResponse.split(';');
+
+      $('#cs1 input[data-id="m-1"]').val(splitted[0]);
+      $('#cs1 input[data-id="m-2"]').val(splitted[1]);
+      $('#cs1 input[data-id="m-3"]').val(splitted[2]);
+
+      $('#cs2 select[data-id="m-4"]').val(splitted[3]);
+      $('#cs2 select[data-id="m-5"]').val(splitted[4]);
+      $('#cs2 select[data-id="m-6"]').val(splitted[5]);
+
+      $('#cs3 input[data-id="m-7"]').val(splitted[6]);
+      $('#cs3 input[data-id="m-8"]').val(splitted[7]);
+      $('#cs3 select[data-id="m-9"]').val(splitted[8]);
+
+      $('#cs4 select[data-id="m-10"]').val(splitted[9]);
+      $('#cs4 select[data-id="m-11"]').val(splitted[10]);
+      $('#cs4 select[data-id="m-12"]').val(splitted[11]);
+      $('#cs4 select[data-id="m-13"]').val(splitted[12]);
+      $('#cs4 input[data-id="m-14"]').prop('checked',parseInt(splitted[13]));
+      $('#cs4 input[data-id="m-15"]').prop('checked',parseInt(splitted[14]));
+
+      $('#cs5 input[data-id="m-16"]').prop('checked',parseInt(splitted[15]));
+      $('#cs5 input[data-id="m-17"]').prop('checked',parseInt(splitted[16]));
+      $('#cs5 input[data-id="m-18"]').prop('checked',parseInt(splitted[17]));
+      $('#cs5 input[data-id="m-19"]').prop('checked',parseInt(splitted[18]));
+      $('#cs5 input[data-id="m-20"]').prop('checked',parseInt(splitted[19]));
+      $('#cs5 input[data-id="m-21"]').prop('checked',parseInt(splitted[20]));
+      $('#cs5 input[data-id="m-22"]').prop('checked',parseInt(splitted[21]));
+
+      switch (splitted[22]) {
+        case 'Office Premium':
+          $('#lic2').prop('checked',true);
+          break;
+        case 'Office Basic':
+          $('#lic1').prop('checked',true);
+          break;
+      }
+
+      $('#m-textarea').text(splitted[23]);
+
+      
+    }
+
   })
   .fail(function(){
     console.log('[Error] Konnte Datei data.json nicht lesen');
